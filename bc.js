e8af79051fd86de90cc2285f6bc5942cfa594463
yosefashmawy <47098834+yosefashmawy@users.noreply.github.com>
2019-02-02 18:07:54 +0200

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "$";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "tk")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Server_{TK}`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+
+
+client.login("NTQxMDY4NjQ0ODM5MzI1Njk2.DzaSjA.fhAOeoOouZJ2_M0uC9T1vtF8qO4");
